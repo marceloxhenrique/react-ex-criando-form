30ca817d0184418321be6ff33f1e01ff82993e02
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-01-26 21:01:34 +0100

    Creat form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
+import React, { useState } from "react";
+
+type InputChangeEvent = React.ChangeEvent<HTMLInputElement>;
+
 const App = () => {
+  const [name, setName] = useState("")
+  const [surname, setLastName] = useState("")
+  const [idade, setAge] = useState(0)
+  
+  const handleNameChange = (event: InputChangeEvent) => {
+    setName(event.target.value)
+  }
+  const handleSurNameChange = (event: InputChangeEvent) => {
+    setLastName(event.target.value)
+  }
+  const handleAgeChange = (event: InputChangeEvent) => {
+    setAge( Number(event.target.value) );
+  }
+
   return (
     <div>
       <div>
         Nome:
-        <input type="text" />
+        <input type="text" value={name} onChange={handleNameChange}/>
       </div>
 
       <div>
         Sobrenome:
-        <input type="text" />
+        <input type="text" value={surname} onChange={handleSurNameChange}/>
       </div>
 
       <div>
         Idade:
-        <input type="text" />
+        <input type="text" value={idade} onChange={handleAgeChange}/>
       </div>
 
       <hr/>
 
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
+      Olá {`${name} ${surname}`}, tudo bem?<br/>
+      Você tem {idade} anos.
     </div>
   );
 }
